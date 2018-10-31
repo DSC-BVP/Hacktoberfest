9a7882f0535e8cbd7378280ced70cc3537316e82
Aditya Dogra <adityadogra9@gmail.com>
2018-10-31 20:27:05 +0530

    ADDED NEW FILE

---
+++ Hacktoberfest/a.java
@@ -0,0 +1,12 @@
+package mypack1;
+class A{
+     int i=10;
+	 String name="ADITYA";
+	 public static void main(String[] args){
+     A a=new A();
+	 System.out.println(a.i);
+	 System.out.println(a.name);
+	 B b=new B();
+	 
+	 }
+	 }
